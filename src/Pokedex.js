60da5d8ad9a4936b2bdf6121d0389cc6791bf8df
ryanleviathan <ryan.m.carreras@gmail.com>
2020-10-30 12:54:35 -0700

    search bar functional

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,21 +4,35 @@ import poke from './assets/poke.gif'
 
 export default class Pokedex extends Component {
     state = {
-        pokemonArray: []
+        pokemonArray: [],
+        search: ''
     }
+
     componentDidMount = async () => {
         const response = await fetch.get('https://alchemy-pokedex.herokuapp.com/api/pokedex');
         this.setState({ pokemonArray: response.body.results })
     }
+
+    handleClick = async () => {
+        const response = await fetch.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.pokemon}`);
+        this.setState({ pokemonArray: response.body.results })
+    }
+
+    handleChange = (e) => {
+        this.setState({ pokemon: e.target.value })
+    }
+
     render() {
         return (
             <>
             <div>
+                <input onChange={this.handleChange} />
+                <button onClick={this.handleClick}>Search by Name</button>
                 {
                 this.state.pokemonArray.length === 0
                 ? <img src={poke} alt="Loading" />
                 : this.state.pokemonArray.map(pokemon =>
-                    <div className="poke-render">
+                    <div key={pokemon.render} className="poke-render">
                         <p className="poke-name">Name: {pokemon.pokemon}</p>
                         <img className="poke-img" src={pokemon.url_image} alt={pokemon.name} />
                         <p className="poke-t1">Type: {pokemon.type_1}</p>
