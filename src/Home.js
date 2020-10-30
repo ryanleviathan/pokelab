a9c5fa2265da5e0b10ae27568b1bb13d16019819
ryanleviathan <ryan.m.carreras@gmail.com>
2020-10-30 09:31:19 -0700

    two pages linked

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
     render() {
@@ -13,7 +14,9 @@ export default class Home extends Component {
                 <p>
                     Explore and enjoy!
                 </p>
+                <Link to="/pokedex">
                 <button className="home-button">Continue</button>
+                </Link>
             </div>
         )
     }
