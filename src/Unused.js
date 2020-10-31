85fca1c85eae55c556cf214a929d29d1c27319d8
ryanleviathan <ryan.m.carreras@gmail.com>
2020-10-31 15:25:53 -0700

    moved things around so the site is deployable by netlify

--- src/Unused.js
+++ src/Unused.js
@@ -1,10 +1,4 @@
-// This is really just an empty file with my radio buttons for sorting by type to cleanup Pokedex.js
-
-// // for radio buttons
-// handleCheck = async () => {
-//     const response = await fetch.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex/types=${this.state.byType}`);
-//     this.setState({ pokemonArray: response.body.results })
-// }
+import React, { Component } from 'react'
 
 // // pokemon types img
 // import bug from './assets/poke-types-sm/bug.png'
@@ -26,54 +20,71 @@
 // import steel from './assets/poke-types-sm/steel.png'
 // import water from './assets/poke-types-sm/water.png'
 
-{/* <label>
-    <div>
-        <input type="radio" value="bug" id="bug"></input>
-        <label for="bug"><img src={bug} alt="bug" /></label>
-        <input type="radio" value="dark" id="dark"></input>
-        <label for="dark"><img src={dark} alt="dark" /></label>
-        <input type="radio" value="dragon" id="dragon"></input>
-        <label for="dragon"><img src={dragon} alt="dragon" /></label>
-    </div>
-    <div>
-        <input type="radio" value="electric" id="electric"></input>
-        <label for="electric"><img src={electric} alt="electric" /></label>
-        <input type="radio" value="fairy" id="fairy"></input>
-        <label for="fairy"><img src={fairy} alt="fairy" /></label>
-        <input type="radio" value="fighting" id="fighting"></input>
-        <label for="fighting"><img src={fighting} alt="fighting" /></label>
-    </div>
-    <div>
-        <input type="radio" value="fire" id="fire"></input>
-        <label for="fire"><img src={fire} alt="fire" /></label>
-        <input type="radio" value="flying" id="flying"></input>
-        <label for="flying"><img src={flying} alt="flying" /></label>
-        <input type="radio" value="ghost" id="ghost"></input>
-        <label for="ghost"><img src={ghost} alt="ghost" /></label>
-    </div>
-    <div>
-        <input type="radio" value="grass" id="grass"></input>
-        <label for="grass"><img src={grass} alt="grass" /></label>
-        <input type="radio" value="ground" id="ground"></input>
-        <label for="ground"><img src={ground} alt="ground" /></label>
-        <input type="radio" value="ice" id="ice"></input>
-        <label for="ice"><img src={ice} alt="ice" /></label>
-    </div>
-    <div>    
-        <input type="radio" value="normal" id="normal"></input>
-        <label for="normal"><img src={normal} alt="normal" /></label>
-        <input type="radio" value="poison" id="poison"></input>
-        <label for="poison"><img src={poison} alt="poison" /></label>
-        <input type="radio" value="psychic" id="psychic"></input>
-        <label for="psychic"><img src={psychic} alt="psychic" /></label>
-    </div>
-    <div>
-        <input type="radio" value="rock" id="rock"></input>
-        <label for="rock"><img src={rock} alt="rock" /></label>
-        <input type="radio" value="steel" id="steel"></input>
-        <label for="steel"><img src={steel} alt="steel" /></label>
-        <input type="radio" value="water" id="water"></input>
-        <label for="water"><img src={water} alt="water" /></label>
-    </div>
-    <button className="button" onClick={this.handleCheck}>Search by Type</button>
-</label> */}
+export default class Unused extends Component {
+    // // for radio buttons
+        // handleCheck = async () => {
+        //     const response = await fetch.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex/types=${this.state.byType}`);
+        //     this.setState({ pokemonArray: response.body.results })
+        // }
+    render() {
+        return (
+            <div>
+                {/* <label>
+                        <div>
+                            <input type="radio" value="bug" id="bug"></input>
+                            <label for="bug"><img src={bug} alt="bug" /></label>
+                            <input type="radio" value="dark" id="dark"></input>
+                            <label for="dark"><img src={dark} alt="dark" /></label>
+                            <input type="radio" value="dragon" id="dragon"></input>
+                            <label for="dragon"><img src={dragon} alt="dragon" /></label>
+                        </div>
+                        <div>
+                            <input type="radio" value="electric" id="electric"></input>
+                            <label for="electric"><img src={electric} alt="electric" /></label>
+                            <input type="radio" value="fairy" id="fairy"></input>
+                            <label for="fairy"><img src={fairy} alt="fairy" /></label>
+                            <input type="radio" value="fighting" id="fighting"></input>
+                            <label for="fighting"><img src={fighting} alt="fighting" /></label>
+                        </div>
+                        <div>
+                            <input type="radio" value="fire" id="fire"></input>
+                            <label for="fire"><img src={fire} alt="fire" /></label>
+                            <input type="radio" value="flying" id="flying"></input>
+                            <label for="flying"><img src={flying} alt="flying" /></label>
+                            <input type="radio" value="ghost" id="ghost"></input>
+                            <label for="ghost"><img src={ghost} alt="ghost" /></label>
+                        </div>
+                        <div>
+                            <input type="radio" value="grass" id="grass"></input>
+                            <label for="grass"><img src={grass} alt="grass" /></label>
+                            <input type="radio" value="ground" id="ground"></input>
+                            <label for="ground"><img src={ground} alt="ground" /></label>
+                            <input type="radio" value="ice" id="ice"></input>
+                            <label for="ice"><img src={ice} alt="ice" /></label>
+                        </div>
+                        <div>    
+                            <input type="radio" value="normal" id="normal"></input>
+                            <label for="normal"><img src={normal} alt="normal" /></label>
+                            <input type="radio" value="poison" id="poison"></input>
+                            <label for="poison"><img src={poison} alt="poison" /></label>
+                            <input type="radio" value="psychic" id="psychic"></input>
+                            <label for="psychic"><img src={psychic} alt="psychic" /></label>
+                        </div>
+                        <div>
+                            <input type="radio" value="rock" id="rock"></input>
+                            <label for="rock"><img src={rock} alt="rock" /></label>
+                            <input type="radio" value="steel" id="steel"></input>
+                            <label for="steel"><img src={steel} alt="steel" /></label>
+                            <input type="radio" value="water" id="water"></input>
+                            <label for="water"><img src={water} alt="water" /></label>
+                        </div>
+                        <button className="button" onClick={this.handleCheck}>Search by Type</button>
+                    </label> */}
+            </div>
+        )
+    }
+}
+
+
+// This is really just an empty file with my radio buttons for sorting by type to cleanup Pokedex.js
+
